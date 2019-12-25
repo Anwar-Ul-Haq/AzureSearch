147775785422e2787fda67448272e10758d14150
Anwar Ul-Haq <aulhaq6@gmail.com>
2019-12-25 20:02:52 +0000

    Add project files.

---
+++ AzureSearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Index = Microsoft.Azure.Search.Models.Index;
+
+namespace AzureSearch
+{
+    class Program
+    {
+
+        private static readonly string searchServiceName = "XXXXXXX"; //Name Azure Search 
+
+        private static readonly string adminApiKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; //admin key
+        static void Main(string[] args)
+        {
+            var serviceClient = CreateSearchServiceClient();
+            CreateIndex(serviceClient);
+
+
+        }
+
+        private static void CreateIndex(SearchServiceClient serviceClient)
+        {
+            var definition = new Index()
+            {
+                Name = "hotels",
+                Fields = FieldBuilder.BuildForType<Hotel>()
+             };
+            serviceClient.Indexes.Create(definition);
+                
+        }
+
+        private static SearchServiceClient CreateSearchServiceClient()
+        {
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+            return serviceClient;
+        }
+    }
+}
